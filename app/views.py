3a85ccbdbf2696def409181fa926c3712be0ea73
David Kang <kang.woohyeok@gmail.com>
2017-07-02 05:58:48 +0000

    Added some dynamic jquery disappear button in /interactive

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from app import app
+from flask import render_template
 
-@app.route('/')
+#@app.route('/')
 @app.route('/index')
 def index():
   return "Hello World"
+
+
+# source of comment: https://pythonprogramming.net/jquery-flask-tutorial/
+@app.route('/interactive/')
+def interactive():
+
+    return render_template('interactive.html')
