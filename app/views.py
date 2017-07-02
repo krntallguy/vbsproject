dd0b9b70054fe169f20b7a5d95257bcb19853df2
Daniel Kim <krntallguy@gmail.com>
2017-07-02 00:02:54 +0000

    fixed README and added GET, PUT to our webservice

--- app/views.py
+++ app/views.py
@@ -1,6 +1,15 @@
-from app import app
+from app import api
+from flask_restful import Resource
+from flask import request
 
-@app.route('/')
-@app.route('/index')
-def index():
-  return "Hello World"
+todos = {}
+
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
