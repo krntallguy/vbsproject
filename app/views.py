acf812de7b6e7f506237ccf7e2d1b4c12f4a8e8c
Daniel Kim <krntallguy@gmail.com>
2017-07-07 07:05:03 +0000

    added backend logic

--- app/views.py
+++ app/views.py
@@ -2,14 +2,32 @@ from app import api
 from flask_restful import Resource
 from flask import request
 
-todos = {}
+player1 = [0,50]
+player2 = [100,50]
+ball = [50,50]
 
-class TodoSimple(Resource):
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
+class PlayerMoves(Resource):
+    def get(self, playernum):
+        if playernum == 1:
+          return {playernum: player1}
+        if playernum == 2:
+          return {playernum: player2}
+        else: return {-1: -1}
 
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
+    def put(self, playernum):
+        direction = request.form['data']
+        if playernum == '1':
+          if direction > 0:
+            player1[1] = player1[1] + 1
+          else:
+            player1[1] = player1[1] - 1
+          return {playernum: player1}
+        if playernum == '2':
+          if direction < 0:
+            player2[1] = player2[1] + 1
+          else:
+            player2[1] = player2[1] - 1
+          return {playernum: player2}
+        return {-1: -1}
 
-api.add_resource(TodoSimple, '/<string:todo_id>')
+api.add_resource(PlayerMoves, '/<string:playernum>')
--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 #!flask/bin/python
 from app import app
-app.run(debug=True)
+app.run(port=int("8000"), debug=True)
